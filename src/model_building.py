fcd6b192570c3889c01a2c2a381370b8ea43239e
SaeedehNasrinJamali <152750087+SaeedehNasrinJamali@users.noreply.github.com>
2025-10-23 19:45:35 -0400

    Update model_building.py

--- src/model_building.py
+++ src/model_building.py
@@ -36,7 +36,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     x, y, test_size=0.2, random_state=42, stratify=y
 )
 
-print(f"✅ Data split complete: Train shape {X_train.shape}, Test shape {X_test.shape}")
 
 # =========================
 # 3) Model Training (XGBoost)
